bf75720ecc39eeb4cb6955b0d5848cc5ea108d05
Francesc Arellano <francesc.arellano@protectfive.com>
2023-03-09 14:01:42 +0100

    commit

--- js/main.js
+++ js/main.js
@@ -85,30 +85,12 @@ $(window).scroll(function() {
       },
     },
   });
-
-  // Testimonials carousel
-  $(".testimonial-carousel").owlCarousel({
-    autoplay: true,
-    smartSpeed: 1000,
-    margin: 25,
-    loop: true,
-    center: true,
-    dots: false,
-    nav: true,
-    navText: [
-      '<i class="bi bi-chevron-left"></i>',
-      '<i class="bi bi-chevron-right"></i>',
-    ],
-    responsive: {
-      0: {
-        items: 1,
-      },
-      768: {
-        items: 2,
-      },
-      992: {
-        items: 3,
-      },
-    },
+  $(window).scroll(function() {
+    var scrollPos = $(this).scrollTop();
+    var elementOffset = $(".mobile-dnone").offset().top;
+    
+    if (scrollPos > elementOffset) {
+      $(".mobile-dnone").removeClass("mobile-dnone");
+    }
   });
 })(jQuery);
